2ce067c60be1839984f803a81dc80bed50537404
edubew <edubew98@gmail.com>
2023-01-12 15:47:42 +0300

    Add basic styling to the calculator app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Calculator from './Components/Calculator';
+
+const App = () => {
+  return (
+    <div>
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
+
