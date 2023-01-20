7132ed2dbf9536ac78b0339ba7f042907421e386
edubew <edubew98@gmail.com>
2023-01-20 23:08:48 +0300

    Add media queries to make the app responsive

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,24 +1,43 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Link } from 'react-router-dom';
+import { FaBars, FaTimes } from 'react-icons/fa';
 
-const Navbar = () => (
-  <header>
-    <Link className="app__title" to="/">
-      Math Magicians
-    </Link>
+const Navbar = () => {
+  const navRef = useRef();
 
-    <nav>
-      <Link className="nav__item" to="/">
-        Home
+  const showNavbar = () => {
+    navRef.current.classList.toggle('responsive__nav');
+  };
+
+  return (
+    <header>
+      <Link className="app__title" to="/">
+        Math Magicians
       </Link>
-      <Link className="nav__item" to="/calculator">
-        Calculator
-      </Link>
-      <Link className="nav__item" to="/quote">
-        Quote
-      </Link>
-    </nav>
-  </header>
-);
+
+      <nav ref={navRef}>
+        <Link className="nav__item" to="/">
+          Home
+        </Link>
+        <Link className="nav__item" to="/calculator">
+          Calculator
+        </Link>
+        <Link className="nav__item" to="/quote">
+          Quote
+        </Link>
+        <button
+          type="submit"
+          onClick={showNavbar}
+          className="nav__btn close__btn"
+        >
+          <FaTimes />
+        </button>
+      </nav>
+      <button type="submit" onClick={showNavbar} className="nav__btn">
+        <FaBars />
+      </button>
+    </header>
+  );
+};
 
 export default Navbar;
