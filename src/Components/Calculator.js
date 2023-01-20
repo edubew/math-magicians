37e6ecf180971ee9b853d31420270614d646294d
edubew <edubew98@gmail.com>
2023-01-20 09:15:35 +0300

    Add styling to the calculator component

--- src/Components/Calculator.js
+++ src/Components/Calculator.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import calculate from '../logic/calculate';
+import math from '../assets/math.jpeg';
 
 const Calculator = () => {
   const [currentState, setState] = useState(0);
@@ -17,87 +18,92 @@ const Calculator = () => {
       : 0
   }`;
   return (
-    <div className="calculator__container">
-      <div className="calculator__output">
-        <span>{result}</span>
+    <div className="calculator__wrapper">
+      <div className="calculator__container">
+        <div className="calculator__output">
+          <span>{result}</span>
+        </div>
+        <button type="submit" onClick={eventHandler}>
+          AC
+        </button>
+        <button type="submit" onClick={eventHandler}>
+          +/-
+        </button>
+        <button type="submit" onClick={eventHandler}>
+          %
+        </button>
+        <button
+          type="submit"
+          onClick={eventHandler}
+          className="calculator__operand"
+        >
+          ÷
+        </button>
+        <button type="submit" onClick={eventHandler}>
+          7
+        </button>
+        <button type="submit" onClick={eventHandler}>
+          8
+        </button>
+        <button type="submit" onClick={eventHandler}>
+          9
+        </button>
+        <button
+          type="submit"
+          onClick={eventHandler}
+          className="calculator__operand"
+        >
+          x
+        </button>
+        <button type="submit" onClick={eventHandler}>
+          4
+        </button>
+        <button type="submit" onClick={eventHandler}>
+          5
+        </button>
+        <button type="submit" onClick={eventHandler}>
+          6
+        </button>
+        <button
+          type="submit"
+          onClick={eventHandler}
+          className="calculator__operand"
+        >
+          -
+        </button>
+        <button type="submit" onClick={eventHandler}>
+          1
+        </button>
+        <button type="submit" onClick={eventHandler}>
+          2
+        </button>
+        <button type="submit" onClick={eventHandler}>
+          3
+        </button>
+        <button
+          type="submit"
+          onClick={eventHandler}
+          className="calculator__operand"
+        >
+          +
+        </button>
+        <button type="submit" onClick={eventHandler} className="span__two">
+          0
+        </button>
+        <button type="submit" onClick={eventHandler}>
+          .
+        </button>
+        <button
+          type="submit"
+          onClick={eventHandler}
+          className="calculator__operand"
+        >
+          =
+        </button>
       </div>
-      <button type="submit" onClick={eventHandler}>
-        AC
-      </button>
-      <button type="submit" onClick={eventHandler}>
-        +/-
-      </button>
-      <button type="submit" onClick={eventHandler}>
-        %
-      </button>
-      <button
-        type="submit"
-        onClick={eventHandler}
-        className="calculator__operand"
-      >
-        ÷
-      </button>
-      <button type="submit" onClick={eventHandler}>
-        7
-      </button>
-      <button type="submit" onClick={eventHandler}>
-        8
-      </button>
-      <button type="submit" onClick={eventHandler}>
-        9
-      </button>
-      <button
-        type="submit"
-        onClick={eventHandler}
-        className="calculator__operand"
-      >
-        x
-      </button>
-      <button type="submit" onClick={eventHandler}>
-        4
-      </button>
-      <button type="submit" onClick={eventHandler}>
-        5
-      </button>
-      <button type="submit" onClick={eventHandler}>
-        6
-      </button>
-      <button
-        type="submit"
-        onClick={eventHandler}
-        className="calculator__operand"
-      >
-        -
-      </button>
-      <button type="submit" onClick={eventHandler}>
-        1
-      </button>
-      <button type="submit" onClick={eventHandler}>
-        2
-      </button>
-      <button type="submit" onClick={eventHandler}>
-        3
-      </button>
-      <button
-        type="submit"
-        onClick={eventHandler}
-        className="calculator__operand"
-      >
-        +
-      </button>
-      <button type="submit" onClick={eventHandler} className="span__two">
-        0
-      </button>
-      <button type="submit" onClick={eventHandler}>
-        .
-      </button>
-      <button
-        type="submit"
-        onClick={eventHandler}
-        className="calculator__operand"
-      >
-        =
-      </button>
+      <div>
+        <img src={math} alt="math icon" className="calculator__image" />
+      </div>
     </div>
   );
 };
